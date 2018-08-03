af4a79025cd6381381e465373f41d1a2c27148b5
Fred <hi@ifred.ru>
2018-08-03 13:21:41 +0300

    add && edit fix

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import store from './store'
 import Home from './views/Home.vue'
 import Book from './views/Book.vue'
+import AddBook from './views/Add.vue'
 import SaveBook from './views/SaveBook.vue'
 
 Vue.use(VueRouter)
@@ -15,7 +16,7 @@ const routes = [
     },
     {
         path: '/add',
-        component: SaveBook,
+        component: AddBook,
         name: 'add'
     },
     {
